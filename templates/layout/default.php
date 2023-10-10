15d00bc7c4f359aa54fde15e0f3e6bbf125b5c7d
tadesseamsalu@gmail.com <Amsalu>
2023-10-10 09:03:53 +0300

    bootstrap css added

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -31,6 +31,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <?= $this->fetch('script') ?>
 </head>
 <body>
